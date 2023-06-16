4eab3bb9e78152c61f706947779aa1e844f15939
Dikeledi-K <nonydikeledi@gmail.com>
2023-06-15 20:14:04 -0700

    my seventh commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,22 @@
 #incude < stdio.h >
 /**
- * Main - Entry point
- * Return : 0 (success)
+ * main - Entry point
+ * return : Always 0 (success)
  */
-int main(void)
+	int main(void)
 {
-int a;
-char b;
-long c;
-long long d;
-float d;
+	int a;
+	char b;
+	long c;
+	long long d;
+	float d;
 
-printf("Size of a char: %lu byte(s)\n", sizeof(b));
-printf("Size of an int: %lu byte(s)\n", sizeof(a));
-printf("Size of a long int: %lu byte(s)\n", sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-printf("Size of a float: %lu byte(s)\n", sizeof(e));
-return (0);
+	printf("Size of a char: %lu byte(s)\n", sizeof(b));
+	printf("Size of an int: %lu byte(s)\n", sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e));
+
+	return (0);
+
 }
