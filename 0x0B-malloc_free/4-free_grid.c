fe6e724ca5a8f2d227b19ef76d5627298a8468d0
Dikeledi-K <nonydikeledi@gmail.com>
2023-07-11 07:49:30 -0700

    my fifth commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * free_grid - ...
+  * 
+  * @height: ...
+  *
+  * @grid: ...
+  *
+  * Return: ...
+  */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+
+}
