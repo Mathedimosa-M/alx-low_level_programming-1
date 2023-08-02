ecdd13c0769f6ffe5ac9f49ec4db649c66fd98da
Dikeledi-K <nonydikeledi@gmail.com>
2023-08-01 20:17:30 -0700

    my first commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of the linked list
- * @h: linked list of type listint_t to be printed
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
@@ -17,3 +17,4 @@ h = h->next;
 return (num);
 
 }
+
